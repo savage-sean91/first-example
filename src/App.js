b8722408088d5ac97d42780a6528b3e5427daaaa
savage-sean91 <savage.sean91@gmail.com>
2022-04-06 20:39:07 -0400

    commit 04/06/2022
    just a commit to save some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import BankAccount from './BankAccount';
+import logo from "./logo.svg";
+import "./App.css";
+import BankAccount from "./BankAccount";
+
+let startingBalance = 1000; // data coming from a database in real situations, from an api or server
+let name = "Sean Savage";
 
 function App() {
   return (
-  <>
-    <h1>My App</h1>
-    <BankAccount />
-  </>
+    <>
+      <h1>My App</h1>
+      <BankAccount startingBalance={startingBalance} name={name} />
+    </>
   );
 }
 
--- src/BankAccount.js
+++ src/BankAccount.js
@@ -1,30 +1,77 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-class BankAccount extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            balance: 0.00
-        };
-    }
+class BankAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: props.startingBalance,
+      amount: "",
+    };
+    //Any function in the component that contains code using the "this" keyword msut be bound (like below)
+    this.deposit = this.deposit.bind(this);
+    this.addHundred = this.addHundred.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+  }
 
-    deposit(){
-        alert('deposited');
-    }
+  addHundred() {
+    /* 
+      Adds 100 to the balance
+      */
 
-    render(){
-        return (
-            <div>
-                <div>Bank Account</div>
-                <div>Balance: ${this.state.balance}</div>
-                < hr/>
-                <section>
-                    <input type="number" />
-                    <button onClick={this.deposit}>Deposit</button>
-                </section>
-            </div>
-        );
-    }
+    // We tell react (using setState) HOW to create the NEXT state
+    // It does not change the CURRENT state
+    this.setState({ balance: this.state.balance + 100 });
+  }
+  handleAmountChange(event) {
+    let numericalValue = parseInt(event.target.value);
+    this.setState({ amount: numericalValue });
+  }
+
+  deposit() {
+    alert("deposited");
+    this.setState({
+      balance: this.state.balance + this.state.amount,
+      amount: "",
+    });
+  }
+  /*
+    * User can enter a number - X
+    * User can click deposit button - X
+    * * Clicking Deposit button will add the value from the input field to the balance 
+    * *Input field should clear
+
+  */
+
+  render() {
+    return (
+      <div>
+        <h1>Bank Account</h1>
+        <h2>{this.props.name}</h2>
+        <div>Balance: ${this.state.balance}</div>
+        <hr />
+        <section>
+          <input
+            type="number"
+            value={this.state.amount}
+            onChange={this.handleAmountChange}
+          />
+          <button onClick={this.deposit}>Deposit</button>
+          <button onClick={this.addHundred}>Add Hundred</button>
+        </section>
+      </div>
+    );
+  }
 }
+
+BankAccount.propTypes = {
+  startingBalance: PropTypes.number,
+  name: PropTypes.string,
+};
+
+BankAccount.defaultProps = {
+  startingBalance: 0,
+  name: "Customer",
+};
 
 export default BankAccount;
